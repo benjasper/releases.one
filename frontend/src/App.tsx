4802e78c4ddce2382dda3ebdccbd26988c30883e
Benjamin Jasper <jasper.benni@gmail.com>
2024-12-08 14:16:08 +0100

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import { Button } from './components/ui/button';
+
+const App: Component = () => {
+	return (
+		<Button>Hello world!</Button>
+	);
+};
+
+export default App;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/benjasper/releases.one
+
+go 1.23.3
